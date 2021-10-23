0331c5981602af10bf78beacdb156709c8c60727
Desoga10 <done2stay@gmail.com>
2021-10-23 11:40:11 +0100

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Signin from './Signin';
+import Signup from './Signup';
+
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Signup /> */}
+      <Signin />
+    </div>
+  );
+}
+
+export default App;
